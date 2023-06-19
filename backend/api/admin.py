f233d5523d4ba5d9cd5aa0c1b9648255b67db206
= <=>
2023-06-19 22:22:17 +0100

    registered the models in the admin app

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Trade, TradeSummary, Fund, Pair
 # Register your models here.
+
+admin.site.register(Pair)
+admin.site.register(Fund)
+admin.site.register(Trade)
+admin.site.register(TradeSummary)
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -19,14 +19,29 @@ class TradeSummarySerializer(serializers.ModelSerializer):
         fields=('amount', 'balance', 'comment')
 
 class TradeSerializer(serializers.ModelSerializer):
+
+    """ 
+    Serializes the Trade model. accesses trade
+    summary models through the related names
+    
+    """
+
     pair = PairSerializer()
     summary = TradeSummarySerializer()
+
     class Meta:
         model=Trade
         fields=('id', 'unit', 'pair', 'open_price', 'opened_date', 'close_price', 'closed_date', 'is_closed', 'summary')
 
+
 class UserSerializer(serializers.ModelSerializer):
     
+    """ 
+    Serializes the User model. accesses the trade 
+    and fund models through the related names
+
+    """
+
     trades = TradeSerializer()
     fund = FundSerializer()
 
@@ -35,41 +50,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields=('id', 'username', 'first_name', 'last_name', 'email', 'fund', 'trades')
 
 
-{
-    token : auth_token,
-    username: user1@gmail.com
-    name: user name,
-    email: user@gmail.com,
-    fund: {amount:100, currency:usd},
-    manage: auth_token
-
-    trades =[
-        {
-            id: uuid
-            unit:1_000_000,
-            pair: USD/EUR
-            open_price: 1.8,
-            open_date: datetime
-            close_price: 1.2,
-            close_date: datetime
-            is_closed:True
-        }
-    ]
-
-    history:
-    [
-        {
-            trade_id: uuid
-            amount: $10
-            balance: $110,
-            comment: profit
-        },
-        {
-            trade_id: uuid
-            amount: -5
-            balance: $105,
-            status: loss
-        }
-    
-    ]
-}
